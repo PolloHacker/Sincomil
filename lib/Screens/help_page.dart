adf67bf6a2cfe7559ec99a48cfa9cc4014b7aad7
PolloHacker <isaac.reyalves@gmail.com>
2023-01-23 18:45:45 -0300

    added mail and phone support to help_page.dart

--- lib/Screens/help_page.dart
+++ lib/Screens/help_page.dart
@@ -4,6 +4,7 @@ import 'package:url_launcher/url_launcher.dart';
 import '../Classes/parent.dart';
 import '../Classes/student.dart';
 import '../Constants/phones.dart';
+import '../Widgets/expandable_list_tile.dart';
 
 class HelpPage extends StatefulWidget {
   const HelpPage({super.key, required this.parent, required this.students});
@@ -23,64 +24,27 @@ class _HelpPageState extends State<HelpPage> {
       ),
       body: ListView.builder(
           //TODO: add support for other schools
-          itemCount: cmbh.length,
+          itemCount: cmrj.length,
           itemBuilder: (context, index) {
-            return ListTile(
-              title: cmbh.entries.map((e) => Text(e.key)).toList()[index],
-              subtitle: cmbh.entries.map((e) {
-                if (e.value.length > 1) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: e.value.map((e) => Text(e)).toList(),
-                  );
-                } else {
-                  return Text(e.value.first);
-                }
-              }).toList()[index],
-              trailing: IconButton(
-                onPressed: () {
-                  final entry = cmbh.entries.map((e) => e.value).toList()[index];
-                  String tel = '';
-                  if (entry.length > 1) {
-                    showDialog<String>(
-                        context: context,
-                        builder: (context) => AlertDialog(
-                              icon: const Icon(Icons.warning_rounded),
-                              title: const Text("Selecione um número para ligar",
-                                  textAlign: TextAlign.center),
-                              content: const Text("Clique fora do diálogo para cancelar",
-                                  textAlign: TextAlign.center),
-                              actions: <Widget>[
-                                Row(
-                                  children: [
-                                    const Spacer(),
-                                    TextButton(
-                                        onPressed: () {
-                                          tel = entry[0];
-                                          launchUrl(Uri.parse("tel:$tel"));
-                                        },
-                                        child: Row(
-                                          children: [const Icon(Icons.call_rounded), Text(entry[0])],
-                                        )),
-                                    const Spacer(),
-                                    TextButton(
-                                        onPressed: () {
-                                          tel = entry[1];
-                                          launchUrl(Uri.parse("tel:$tel"));
-                                        },
-                                        child: Row(
-                                          children: [const Icon(Icons.call_rounded), Text(entry[1])],
-                                        )),
-                                    const Spacer(),
-                                  ],
-                                )
-                              ],
-                            ));
-                  }
-                },
-                icon: const Icon(Icons.call_rounded),
-              ),
-            );
+            return ExpandableListTile(
+              title: cmrj.entries.map((e) => Text(e.key)).toList()[index],
+                child: Column(
+                  children: cmrj.entries.map((i) =>
+                      i.value.asMap().entries.map((e) =>
+                          ListTile(title: Text(e.value),
+                            trailing: IconButton(
+                              onPressed: () {
+                                if (e.value.contains("@")) {
+                                  launchUrl(Uri.parse("mailto:${e.value}"));
+                                } else {
+                                  launchUrl(Uri.parse("tel:${e.value}"));
+                                }
+                              },
+                              icon: Icon(e.value.contains("@") ? Icons.email_rounded : Icons.phone_rounded)
+                            ),)
+                      ).toList()
+                  ).toList()[index],
+                ));
           }),
     );
   }
