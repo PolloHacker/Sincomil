68995c30076ade11b53df5adb8c0cf188cb8ac60
PolloHacker <isaac.reyalves@gmail.com>
2023-01-24 13:32:52 -0300

    simplified main.dart;

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,13 @@ import 'package:flutter/services.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:provider/provider.dart';
+
 import 'package:sincomil/Constants/shared_constants.dart';
 import 'package:sincomil/Provider/app_settings.dart';
 import 'package:sincomil/Screens/finger_page.dart';
 import 'package:sincomil/Screens/login_page.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-
 import 'Constants/constants.dart';
 import 'Screens/welcome_page.dart';
 
@@ -50,42 +49,22 @@ class MyApp extends StatelessWidget {
                   create: (context) => AppSettings(isDark, useFinger, useCard),
                   builder: (context, snapshot) {
                     final appSettings = Provider.of<AppSettings>(context);
-                    if (appSettings.fingerprint == "true") {
-                      return MaterialApp(
-                        builder: (context, child) => ResponsiveWrapper.builder(
-                            ClampingScrollWrapper.builder(context, child!),
-                            maxWidth: 1200,
-                            minWidth: 480,
-                            defaultScale: true,
-                            breakpoints: [
-                              const ResponsiveBreakpoint.resize(480, name: MOBILE),
-                              const ResponsiveBreakpoint.autoScale(800, name: TABLET),
-                              const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-                              const ResponsiveBreakpoint.autoScale(2460, name: '4K'),
-                            ]),
-                        theme: appSettings.currentTheme,
-                        home: const FingerPage(),
-                        debugShowCheckedModeBanner: false,
-                      );
-                    } else {
-                      return MaterialApp(
-                        builder: (context, child) =>
-                            ResponsiveWrapper.builder(ClampingScrollWrapper.builder(context, child!),
-                                maxWidth: 1200,
-                                minWidth: 480,
-                                defaultScale: true,
-                                breakpoints: [
-                                  const ResponsiveBreakpoint.resize(480, name: MOBILE),
-                                  const ResponsiveBreakpoint.autoScale(800, name: TABLET),
-                                  const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-                                  const ResponsiveBreakpoint.autoScale(2460, name: '4K'),
-                                ],
-                                background: Container(color: whiteSecondary)),
-                        theme: appSettings.currentTheme,
-                        home: const WelcomePage(),
-                        debugShowCheckedModeBanner: false,
-                      );
-                    }
+                    return MaterialApp(
+                      builder: (context, child) => ResponsiveWrapper.builder(
+                          ClampingScrollWrapper.builder(context, child!),
+                          maxWidth: 1200,
+                          minWidth: 480,
+                          defaultScale: true,
+                          breakpoints: [
+                            const ResponsiveBreakpoint.resize(480, name: MOBILE),
+                            const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+                            const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+                            const ResponsiveBreakpoint.autoScale(2460, name: '4K'),
+                          ]),
+                      theme: appSettings.currentTheme,
+                      home: appSettings.fingerprint == "true" ? const FingerPage() : const WelcomePage(),
+                      debugShowCheckedModeBanner: false,
+                    );
                   });
             } else {
               return ChangeNotifierProvider(
@@ -106,16 +85,6 @@ class MyApp extends StatelessWidget {
                               ],
                               background: Container(color: whiteSecondary)),
                       theme: settings.currentTheme,
-                      localizationsDelegates: const [
-                        GlobalMaterialLocalizations.delegate,
-                        GlobalWidgetsLocalizations.delegate,
-                        GlobalCupertinoLocalizations.delegate,
-                      ],
-                      supportedLocales: const [
-                        Locale('en'), // English
-                        Locale('es'), // Spanish
-                        Locale('pt') // Portuguese
-                      ],
                       home: const LoginPage(),
                       debugShowCheckedModeBanner: false,
                     );
